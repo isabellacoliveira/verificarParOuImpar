c99f993f413396e7c591c9ecb8db521c05db978c
Isabella Cruz de Oliveira <112203324+isabellacoliveira@users.noreply.github.com>
2022-10-10 14:45:29 -0300

    Add files via upload

---
+++ LpExercicio2.java
@@ -0,0 +1,22 @@
+package linguagemDeProgamacao;
+
+import java.util.Scanner; 
+
+public class LpExercicio2 {
+	public static void main(String[] args) {
+	
+		Scanner uwu = new Scanner(System.in); 
+		
+				int numero;
+				System.out.println("Digite um número: "); 
+				numero = uwu.nextInt(); 
+				
+				if (numero % 2 == 0) {
+						System.out.println("este número é par");
+					} else {
+						System.out.println("este número é impar");
+			    } 
+		      }
+
+	}
+
